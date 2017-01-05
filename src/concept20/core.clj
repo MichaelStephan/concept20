f2a42999c4a6dc63b955fde071b30b6e89b9dbb5
Michael Stephan <michael.stephan@sap.com>
2017-01-05 17:19:13 +0100

    update

--- src/concept20/core.clj
+++ src/concept20/core.clj
@@ -4,7 +4,7 @@
 
 ; IDEAs
 
-; * when service A (=caller) calls service B (=callee) caller adds signed nonce (=proof of identity) into ctx
+; * when service A (=originator) calls service B (=callee) originator adds signed nonce (=proof of identity) into ctx
 ; * ctx elements may be subject to ttl rules, e.g. a ctx element can be configured to only survive 5 hops until it is dropped
 ; * ctx element may be added to be readable by certain service only (encrypted using public key)
 ; * on service startup services register themselves among each other and exchange secrets
@@ -20,13 +20,15 @@
           (get-in ctx [:sec/claims :yaas/saas])) ctx))
 
 (defn subscription [ctx]
-  (get-in (last ctx) [:sec/claims :yaas/subscription]))
-
-(defn measure [yaas ctx key value]
-  (swap! yaas update-in [:yaas/measurements] conj [(subscription ctx) key value]))
+  (some (fn [ctx]
+          (get-in ctx [:sec/claims :yaas/subscription]))
+        (reverse ctx)))
 
 (defn claim-saas [ctx]
   (conj ctx {:sec/claims {:yaas/saas true}}))
+
+(defn claim-originator [ctx originator]
+  (conj ctx {:sec/claims {:service/originator originator}}))
 
 (defn user-id [ctx]
   (some (fn [ctx]
@@ -39,23 +41,27 @@
         (reverse ctx)))
 
 (defn roles [ctx]
-  ; does it really make sense to combine all roles? do we grant too much privileges to the caller?
+  ; does it really make sense to combine all roles? do we grant too much privileges to the originator?
   (apply clojure.set/union (map (fn [ctx]
                                   (get-in ctx [:sec/claims :sec/roles]))
                                 ctx)))
 
 ; platform 
 
-(defn load-credentials [yaas credentials]
-  (get-in @yaas [:yaas/authentication-db (dissoc credentials :sec/password)]))
+(defn measure [yaas ctx key value]
+  (swap! yaas update-in [:yaas/measurements] conj [(subscription ctx) key value]))
 
-(defn authenticate [yaas {:keys [:sec/username :sec/password] :as credentials}]
+(defn load-credentials [yaas credentials]
+  (get-in @yaas [:yaas/authentication-db (dissoc credentials :yaas/subscription :sec/password)]))
+
+(defn authenticate [yaas {:keys [:sec/username :sec/password :yaas/subscription] :as credentials}]
   [{:sec/claims (let [{stored-password :sec/password :as stored-credentials} (load-credentials yaas credentials)]
                   (if (= stored-password password)
                     (-> stored-credentials
                         (dissoc :sec/password)
                         (assoc :sec/username username)
-                        (assoc :sec/provider "https://sap-identity-provider"))
+                        (assoc :sec/provider "https://sap-identity-provider")
+                        (assoc :yaas/subscription subscription))
                     (throw+ {:type ::not-authenticated :hint "invalid password"})))
     :sec/signature ""}])
 
@@ -70,6 +76,11 @@
                       (apply clojure.set/union (for [role (roles ctx)]
                                                  (get-in @yaas [:yaas/config-db (tenant ctx) :config/authorization role] #{})))
                       requested-scope)))
+
+(defn check-authorized? [yaas service function ctx requested-scope]
+  (when-not (authorized? yaas service function ctx requested-scope)
+    (throw+ {:type ::not-authorized :hint (str service "/" function)}))
+  true)
 
 (defn call [yaas {:keys [:yaas/service] :as service-description} function ctx & args]
   (apply service yaas function ctx args))
@@ -92,34 +103,65 @@
   (doall (for [[id uri] (get services :yaas/services)]
            (swap! yaas update-in [:yaas/config-db (tenant ctx) :config/discovery id] conj {:yaas/subscription subscription-id
                                                                                            :rule (fn [ctx]
-                                                                                                   {:yaas/service uri
-                                                                                                    :yaas/subscription subscription-id})}))))
+                                                                                                   (when (= (subscription ctx) subscription-id)
+                                                                                                     {:yaas/service uri
+                                                                                                      :yaas/subscription subscription-id}))}))))
 
-(defn subscribe [yaas ctx service-bundle-id]
-  (let [service :hybris/platform function :hybris/subscription-service-subscribe
-        subscription-id (-> (java.util.UUID/randomUUID) str keyword)
-        subscription {:service-bundle/id service-bundle-id
-                      :subscription/state {}
-                      :subscription/created (System/currentTimeMillis)}]
-    (if (authorized? yaas service function ctx #{:scope/subscription-manage})
-      (if-let [service-bundle (get-in @yaas [:yaas/service-bundles service-bundle-id])]
-        (doto yaas
-          (swap! assoc-in [:yaas/subscriptions (tenant ctx) subscription-id] subscription)
-          (provision ctx service-bundle (assoc subscription :subscription/id subscription-id))
-          (announce ctx service-bundle subscription-id))
-        (throw+ {:type ::not-found :hint (str "service bundle '" service-bundle-id "'not found")}))
-      (throw+ {:type ::not-authorized :hint (str service "/" function)}))))
+(defn load-service-bundle [yaas id]
+  (or (get-in @yaas [:yaas/service-bundles id])
+      (throw+ {:type ::not-found :entity :yaas/service-bundle :hint id})))
+
+(defn new-subscription [parent-subscription-id service-bundle-id]
+  (cond-> {:subscription/id (-> (java.util.UUID/randomUUID) str keyword)
+           :service-bundle/id service-bundle-id
+           :subscription/state {}
+           :subscription/created (System/currentTimeMillis)}
+    parent-subscription-id (assoc :subscription/parent parent-subscription-id)))
+
+(defn save-subscription [yaas ctx {:keys [:subscription/id] :as subscription}]
+  (swap! yaas assoc-in [:yaas/subscriptions (tenant ctx) id] subscription))
+
+(defn load-service [yaas id]
+  (or (get-in @yaas [:yaas/services id])
+      (throw+ {:type ::not-found :entity :yaas/service :hint id})))
+
+(defn subscribe
+  ([yaas ctx service-bundle-id]
+   (subscribe  yaas ctx nil service-bundle-id))
+  ([yaas ctx parent-subscription-id service-bundle-id]
+   (check-authorized? yaas :hybris/platform :hybris/subscription-service-subscribe ctx #{:scope/subscription-manage})
+   (let [{:keys [:subscription/id] :as subscription} (new-subscription parent-subscription-id service-bundle-id)
+         {:keys [yaas/services] :as service-bundle} (load-service-bundle yaas service-bundle-id)]
+     (doto yaas
+       (save-subscription ctx subscription)
+       (provision ctx service-bundle subscription)
+       (announce ctx service-bundle id))
+     (doall (for [[service] services]
+              (doall (for [service-bunde-id (:yaas/depends-on (load-service yaas service))]
+                       (subscribe yaas ctx id service-bunde-id)))))
+     id)))
 
 ; top level applications
 
 (defn product-service [yaas function ctx & args]
-  (measure yaas ctx ::api-call 1)
+  (measure yaas ctx [:hybris/product-service-v1 function :api-call] 1)
   (condp = function
-    :hybris/product-service-get (if (authorized? yaas :hybris/product-service function ctx #{:scope/product-read})
-                                  (let [[id] args]
-                                    [{:id id
-                                      :name "banana"}])
-                                  (throw+ {:type ::not-authorized :hint (str :hybris/product-service "/" function)}))
+    :hybris/product-service-get (let [[id] args
+                                      data-storage-service (lookup yaas ctx :hybris/data-storage-service-v1)
+                                      ctx (claim-originator :hybris/product-service-v1)]
+                                  (check-authorized? yaas :hybris/product-service-v1 function ctx #{:scope/product-read})
+                                  (data-storage-service :hybris/data-storage-get ctx id))
+    (throw+ {:type ::function-not-found :hint function})))
+
+(defn data-storage-service [yaas function ctx & args]
+  (measure yaas ctx [:hybris/data-storage-service-v1 function :api-call] 1)
+  (condp = function
+    :hybris/data-storage-get (let [[id] args
+                                   data-storage-service (lookup yaas ctx :hybris/data-storage-service-v1)
+                                   ctx (claim-originator :hybris/data-storage-service-v1)]
+                               (check-authorized? yaas :hybris/data-storage-service-v1 function ctx #{:scope/data-storage-read})
+                               [{:id id
+                                 :name "banana"}])
     (throw+ {:type ::function-not-found :hint function})))
 
 (defn category-service [yaas function ctx & args])
@@ -130,9 +172,9 @@
     (doall (for [service-bundle service-bundles]
              (subscribe yaas ctx service-bundle)))))
 
-(defn saas-ui-browser [yaas credentials]
+(defn saas-product-content-management-ui-browser [yaas credentials]
   (let [ctx (->> (authenticate yaas credentials)
                  (authorize yaas #{:role/product-manager})
                  (claim-saas))
-        product-service (lookup yaas ctx :hybris/product-service)]
+        product-service (lookup yaas ctx :hybris/product-service-v1)]
     (product-service :hybris/product-service-get ctx :sku123)))
--- test/concept20/core_test.clj
+++ test/concept20/core_test.clj
@@ -3,27 +3,34 @@
             [concept20.core :refer :all]))
 
 (defn yaas []
-  (atom
-   {:yaas/service-bundles {::product-content-management-v1 {:yaas/services [[:hybris/product-service concept20.core/product-service]]}
-                           ::product-content-management-v2 {:yaas/services [[:hybris/product-service concept20.core/product-service]
-                                                                            [:hybris/category-service concept20.core/category-service]]}}
-    :yaas/subscriptions {}
-    :yaas/measurements []
-    :yaas/authentication-db {{:sec/username "MichaelStephan1982"
-                              :sec/purpose "e-commmerce backoffice"} {:sec/id "6ba7b810-9dad-11d1-80b4-00c04fd430c8"
-                                                                      :sec/password "123"
-                                                                      :sec/tenant ::sap
-                                                                      :sec/full-username "Michael Stephan"
-                                                                      :sec/mail "michael.stephan@sap.com"}}
-    :yaas/authorization-db {"6ba7b810-9dad-11d1-80b4-00c04fd430c8" {:sec/roles #{:role/product-manager :role/administrator}}}
-    :yaas/config-db {::sap {:config/authorization {:role/product-manager #{:scope/product-read}
-                                                   :role/administrator #{:scope/subscription-manage}}
-                            :config/discovery {:hybris/product-service []}}}}))
+  (atom {:yaas/service-bundles {::data-storage-v1 {:yaas/services [[:hybris/data-storage-service-v1 concept20.core/data-storage-service]]}
+                                ::product-content-management-v1 {:yaas/services [[:hybris/product-service-v1 concept20.core/product-service]]}
+                                ::product-content-management-v2 {:yaas/services [[:hybris/product-service-v1 concept20.core/product-service]
+                                                                                 [:hybris/category-service-v2 concept20.core/category-service]]}}
+         :yaas/services {:hybris/data-storage-service-v1 {:sec/scopes [:scope/data-storage-read :scope/data-storage-manage]
+                                                          :yaas/depends-on []}
+                         :hybris/product-service-v1 {:sec/scopes [:scope/product-read :scope/product-manage]
+                                                     :yaas/depends-on [::data-storage-v1]}
+                         :hybris/category-service-v2 {:sec/scopes [:scope/product-read :scope/product-manage]
+                                                      :yaas/depends-on [::data-storage-v1]}}
+         :yaas/subscriptions {}
+         :yaas/measurements []
+         :yaas/authentication-db {{:sec/username "MichaelStephan1982"
+                                   :sec/purpose "e-commmerce backoffice"} {:sec/id "6ba7b810-9dad-11d1-80b4-00c04fd430c8"
+                                                                           :sec/password "123"
+                                                                           :sec/tenant ::sap
+                                                                           :sec/full-username "Michael Stephan"
+                                                                           :sec/mail "michael.stephan@sap.com"}}
+         :yaas/authorization-db {"6ba7b810-9dad-11d1-80b4-00c04fd430c8" {:sec/roles #{:role/product-manager :role/administrator}}}
+         :yaas/config-db {::sap {:config/authorization {:role/product-manager #{:scope/product-read}
+                                                        :role/administrator #{:scope/subscription-manage}}
+                                 :config/discovery {:hybris/product-service-v1 []}}}}))
 
 (deftest test-saas-ui-browser
   (testing "Calling service from saas ui from customer's browser"
     (let [yaas (yaas)
-          credentials {:sec/username "MichaelStephan1982" :sec/password "123" :sec/purpose "e-commmerce backoffice"}]
-      (subscription-ui-browser yaas credentials [::product-content-management-v1 ::product-content-management-v2]) ;; TODO can I be subscribed to two bundles containing the same service
-      (saas-ui-browser yaas credentials)
+          credentials {:sec/username "MichaelStephan1982" :sec/password "123" :sec/purpose "e-commmerce backoffice"}
+          subscriptions (subscription-ui-browser yaas credentials [::product-content-management-v1 ::product-content-management-v2])]
+      (println subscriptions)
+      (saas-product-content-management-ui-browser yaas (assoc credentials :yaas/subscription (second subscriptions))) ; does it make sense to propagate the subscription id along a call. TODO make sure that implict subscriptions are automatically taken into consideration !!! currently responsible for not not working
       (clojure.pprint/pprint @yaas))))
