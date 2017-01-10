d2ff812ed29c8fe9f725571178ebc63ac779de38
Michael Stephan <michael.stephan@sap.com>
2017-01-10 13:30:58 +0100

    update

--- src/concept20/core.clj
+++ src/concept20/core.clj
@@ -4,31 +4,26 @@
 
 ; IDEAs
 
-; * when service A (=originator) calls service B (=callee) originator adds signed nonce (=proof of identity) into ctx
+; * when service A (=originator) calls service B (=callee) originator adds signed nonce (= proof of identity) into ctx
 ; * ctx elements may be subject to ttl rules, e.g. a ctx element can be configured to only survive 5 hops until it is dropped
 ; * ctx element may be added to be readable by certain service only (encrypted using public key)
 ; * on service startup services register themselves among each other and exchange secrets
 
-; TODOs
-; * :config/discovery contains map with service identifier used as keys. The key should become another map to allow for all items to be looked up, not only services 
-
-
 ;context helper
 
-(defn saas? [ctx]
-  (some (fn [ctx]
-          (get-in ctx [:sec/claims :yaas/saas])) ctx))
-
-(defn subscription [ctx]
+(defn subscription "Get subscription identifier" [ctx]
   (some (fn [ctx]
           (get-in ctx [:sec/claims :yaas/subscription]))
         (reverse ctx)))
 
-(defn claim-saas [ctx]
-  (conj ctx {:sec/claims {:yaas/saas true}}))
-
 (defn claim-originator [ctx originator]
-  (conj ctx {:sec/claims {:service/originator originator}}))
+  ; TODO in order to set the originator claim a service needs to authenticate itself
+  (conj ctx {:sec/claims {:service/originator  originator}}))
+
+(defn originator [ctx]
+  (some (fn [ctx]
+          (get-in ctx [:sec/claims :service/originator]))
+        (reverse ctx)))
 
 (defn user-id [ctx]
   (some (fn [ctx]
@@ -71,11 +66,25 @@
       (conj ctx {:sec/claims {:sec/roles active-roles}})
       (throw+ {:type ::not-authorized :hint "requested roles not assigned"}))))
 
+(defn load-service-bundle [yaas id]
+  (or (get-in @yaas [:yaas/service-bundles id])
+      (throw+ {:type ::not-found :entity :yaas/service-bundle :hint id})))
+
 (defn authorized? [yaas service function ctx requested-scope]
-  (= requested-scope (clojure.set/intersection
-                      (apply clojure.set/union (for [role (roles ctx)]
-                                                 (get-in @yaas [:yaas/config-db (tenant ctx) :config/authorization role] #{})))
-                      requested-scope)))
+  (let [scopes-from-roles (apply clojure.set/union (for [role (roles ctx)]
+                                                     (get-in @yaas [:yaas/config-db (tenant ctx) :config/authorization role] #{})))
+        originator (originator ctx)
+        {:keys [sec/scopes yaas/depends-on]} (get-in @yaas [:yaas/services originator])
+        scopes-from-originator  (apply clojure.set/union (for [service-bundle-id depends-on]
+                                                           (apply clojure.set/union (for [[service-id] (:yaas/services (load-service-bundle yaas service-bundle-id))]
+                                                                                      (get-in @yaas [:yaas/services service-id :sec/scopes])))))]
+
+    (= requested-scope (clojure.set/intersection
+                        (clojure.set/union scopes-from-roles ; add the scopes granted by the user's role assignment 
+                                           scopes-from-originator ; add scopes from all yaas services the current originator is dependent from
+                                           scopes ; add the scopes assigned to the originator
+)
+                        requested-scope))))
 
 (defn check-authorized? [yaas service function ctx requested-scope]
   (when-not (authorized? yaas service function ctx requested-scope)
@@ -86,37 +95,42 @@
   (apply service yaas function ctx args))
 
 (defn lookup [yaas ctx service]
-  (if-let [{:keys [:yaas/subscription] :as service-description} (let [filters (get-in @yaas [:yaas/config-db (tenant ctx) :config/discovery service])]
+  (if-let [{:keys [:yaas/subscription] :as service-description} (let [filters (get-in @yaas [:yaas/config-db (tenant ctx) :config/discovery :yaas/services service])]
                                                                   (some (fn [{:keys [rule]}]
                                                                           (rule ctx)) filters))]
     (fn [function ctx & args]
       (apply call yaas service-description function (conj ctx {:sec/claims {:yaas/subscription subscription
                                                                             :sec/signature ""}}) args))
-    (throw+ {:type ::service-not-found :hint service})))
+    (throw+ {:type ::not-found :entity :yaas/service :hint service})))
 
 (defn provision [yaas ctx services {:keys [:subscription/id]}]
   (doall (for [[service-type services] services]
            (doall (for [[type] services]
                     (swap! yaas assoc-in [:yaas/subscriptions (tenant ctx) id :subscription/state service-type type] :provision/done))))))
 
+(defn load-subscription [yaas ctx id]
+  (or (get-in @yaas [:yaas/subscriptions (tenant ctx) id])
+      (throw+ {:type ::not-found :entity :yaas/subscription :hint id})))
+
 (defn announce [yaas ctx services subscription-id]
-  (doall (for [[id uri] (get services :yaas/services)]
-           (swap! yaas update-in [:yaas/config-db (tenant ctx) :config/discovery id] conj {:yaas/subscription subscription-id
-                                                                                           :rule (fn [ctx]
-                                                                                                   (when (= (subscription ctx) subscription-id)
-                                                                                                     {:yaas/service uri
-                                                                                                      :yaas/subscription subscription-id}))}))))
+  (let [{:keys [:subscription/root]} (load-subscription yaas ctx subscription-id)]
+    (doall (for [[id uri] (get services :yaas/services)]
+             (swap! yaas update-in [:yaas/config-db (tenant ctx) :config/discovery :yaas/services id] conj {:yaas/subscription subscription-id
+                                                                                                            :rule (fn [ctx]
+                                                                                                                    (when (= (subscription ctx) root)
+                                                                                                                      {:yaas/service uri
+                                                                                                                       :yaas/subscription subscription-id}))})))))
 
-(defn load-service-bundle [yaas id]
-  (or (get-in @yaas [:yaas/service-bundles id])
-      (throw+ {:type ::not-found :entity :yaas/service-bundle :hint id})))
-
-(defn new-subscription [parent-subscription-id service-bundle-id]
-  (cond-> {:subscription/id (-> (java.util.UUID/randomUUID) str keyword)
-           :service-bundle/id service-bundle-id
-           :subscription/state {}
-           :subscription/created (System/currentTimeMillis)}
-    parent-subscription-id (assoc :subscription/parent parent-subscription-id)))
+(defn new-subscription [root-subscription-id parent-subscription-id service-bundle-id]
+  (let [id (-> (java.util.UUID/randomUUID) str keyword)]
+    (cond-> {:subscription/id id
+             :subscription/root (if root-subscription-id
+                                  root-subscription-id
+                                  id)
+             :service-bundle/id service-bundle-id
+             :subscription/state {}
+             :subscription/created (System/currentTimeMillis)}
+      parent-subscription-id (assoc :subscription/parent parent-subscription-id))))
 
 (defn save-subscription [yaas ctx {:keys [:subscription/id] :as subscription}]
   (swap! yaas assoc-in [:yaas/subscriptions (tenant ctx) id] subscription))
@@ -127,10 +141,10 @@
 
 (defn subscribe
   ([yaas ctx service-bundle-id]
-   (subscribe  yaas ctx nil service-bundle-id))
-  ([yaas ctx parent-subscription-id service-bundle-id]
+   (subscribe  yaas ctx nil nil service-bundle-id))
+  ([yaas ctx root-subscription-id parent-subscription-id service-bundle-id]
    (check-authorized? yaas :hybris/platform :hybris/subscription-service-subscribe ctx #{:scope/subscription-manage})
-   (let [{:keys [:subscription/id] :as subscription} (new-subscription parent-subscription-id service-bundle-id)
+   (let [{:keys [:subscription/id] :as subscription} (new-subscription root-subscription-id parent-subscription-id service-bundle-id)
          {:keys [yaas/services] :as service-bundle} (load-service-bundle yaas service-bundle-id)]
      (doto yaas
        (save-subscription ctx subscription)
@@ -138,7 +152,7 @@
        (announce ctx service-bundle id))
      (doall (for [[service] services]
               (doall (for [service-bunde-id (:yaas/depends-on (load-service yaas service))]
-                       (subscribe yaas ctx id service-bunde-id)))))
+                       (subscribe yaas ctx id id service-bunde-id)))))
      id)))
 
 ; top level applications
@@ -148,21 +162,19 @@
   (condp = function
     :hybris/product-service-get (let [[id] args
                                       data-storage-service (lookup yaas ctx :hybris/data-storage-service-v1)
-                                      ctx (claim-originator :hybris/product-service-v1)]
+                                      ctx (claim-originator ctx :hybris/product-service-v1)]
                                   (check-authorized? yaas :hybris/product-service-v1 function ctx #{:scope/product-read})
                                   (data-storage-service :hybris/data-storage-get ctx id))
-    (throw+ {:type ::function-not-found :hint function})))
+    (throw+ {:type ::not-found :entity :service/function :hint function})))
 
 (defn data-storage-service [yaas function ctx & args]
   (measure yaas ctx [:hybris/data-storage-service-v1 function :api-call] 1)
   (condp = function
-    :hybris/data-storage-get (let [[id] args
-                                   data-storage-service (lookup yaas ctx :hybris/data-storage-service-v1)
-                                   ctx (claim-originator :hybris/data-storage-service-v1)]
+    :hybris/data-storage-get (let [[id] args]
                                (check-authorized? yaas :hybris/data-storage-service-v1 function ctx #{:scope/data-storage-read})
                                [{:id id
                                  :name "banana"}])
-    (throw+ {:type ::function-not-found :hint function})))
+    (throw+ {:type :not-found :entity :service/function :hint function})))
 
 (defn category-service [yaas function ctx & args])
 
@@ -174,7 +186,6 @@
 
 (defn saas-product-content-management-ui-browser [yaas credentials]
   (let [ctx (->> (authenticate yaas credentials)
-                 (authorize yaas #{:role/product-manager})
-                 (claim-saas))
+                 (authorize yaas #{:role/product-manager}))
         product-service (lookup yaas ctx :hybris/product-service-v1)]
     (product-service :hybris/product-service-get ctx :sku123)))
--- test/concept20/core_test.clj
+++ test/concept20/core_test.clj
@@ -3,16 +3,16 @@
             [concept20.core :refer :all]))
 
 (defn yaas []
-  (atom {:yaas/service-bundles {::data-storage-v1 {:yaas/services [[:hybris/data-storage-service-v1 concept20.core/data-storage-service]]}
+  (atom {:yaas/services {:hybris/data-storage-service-v1 {:sec/scopes #{:scope/data-storage-read :scope/data-storage-manage}
+                                                          :yaas/depends-on []}
+                         :hybris/product-service-v1 {:sec/scopes #{:scope/product-read :scope/product-manage}
+                                                     :yaas/depends-on [::data-storage-v1]}
+                         :hybris/category-service-v2 {:sec/scopes #{:scope/product-read :scope/product-manage}
+                                                      :yaas/depends-on [::data-storage-v1]}}
+         :yaas/service-bundles {::data-storage-v1 {:yaas/services [[:hybris/data-storage-service-v1 concept20.core/data-storage-service]]}
                                 ::product-content-management-v1 {:yaas/services [[:hybris/product-service-v1 concept20.core/product-service]]}
                                 ::product-content-management-v2 {:yaas/services [[:hybris/product-service-v1 concept20.core/product-service]
                                                                                  [:hybris/category-service-v2 concept20.core/category-service]]}}
-         :yaas/services {:hybris/data-storage-service-v1 {:sec/scopes [:scope/data-storage-read :scope/data-storage-manage]
-                                                          :yaas/depends-on []}
-                         :hybris/product-service-v1 {:sec/scopes [:scope/product-read :scope/product-manage]
-                                                     :yaas/depends-on [::data-storage-v1]}
-                         :hybris/category-service-v2 {:sec/scopes [:scope/product-read :scope/product-manage]
-                                                      :yaas/depends-on [::data-storage-v1]}}
          :yaas/subscriptions {}
          :yaas/measurements []
          :yaas/authentication-db {{:sec/username "MichaelStephan1982"
@@ -24,13 +24,12 @@
          :yaas/authorization-db {"6ba7b810-9dad-11d1-80b4-00c04fd430c8" {:sec/roles #{:role/product-manager :role/administrator}}}
          :yaas/config-db {::sap {:config/authorization {:role/product-manager #{:scope/product-read}
                                                         :role/administrator #{:scope/subscription-manage}}
-                                 :config/discovery {:hybris/product-service-v1 []}}}}))
+                                 :config/discovery {}}}}))
 
 (deftest test-saas-ui-browser
   (testing "Calling service from saas ui from customer's browser"
     (let [yaas (yaas)
           credentials {:sec/username "MichaelStephan1982" :sec/password "123" :sec/purpose "e-commmerce backoffice"}
           subscriptions (subscription-ui-browser yaas credentials [::product-content-management-v1 ::product-content-management-v2])]
-      (println subscriptions)
-      (saas-product-content-management-ui-browser yaas (assoc credentials :yaas/subscription (second subscriptions))) ; does it make sense to propagate the subscription id along a call. TODO make sure that implict subscriptions are automatically taken into consideration !!! currently responsible for not not working
+      (saas-product-content-management-ui-browser yaas (assoc credentials :yaas/subscription (second subscriptions)))
       (clojure.pprint/pprint @yaas))))
